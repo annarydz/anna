c87249d8daecc374f9b0f60e1aba45fe3b53dc97
Anna <rjuwl2017@gmail.com>
2024-11-14 15:55:31 +0100

    Zmiany do złożenia: 	zmieniono:       podzielnosc.cpp

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {	int x,y;
 	cin>>x>>y;
-	cout<<x<<" "<<y;
+	if(x>0 && y>0)
+	{	cout<<x<<" "<<y;
+	}
+	else
+	{	cout<<"Nie podano dodatnich wartosci"<<endl;
+	}
 return 0;
 }
